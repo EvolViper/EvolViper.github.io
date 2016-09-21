0f1cd07cd85b5aa01900ff7b43fc64873aa36d19
EvolViper <evol.viper@gmail.com>
2016-09-21 17:09:57 +0300

    advanced

---
+++ adaptive/js/script.js
@@ -0,0 +1,16 @@
+(function() {
+    var navIcon = document.getElementById("show-nav");
+    var menu = document.getElementById("menu");
+    var showMenu = false;
+    navIcon.onclick = function(evt) {
+        evt.preventDefault();
+        if (showMenu) {
+            menu.style.display = "none";
+            showMenu = false;
+        }
+        else {
+            menu.style.display = "block";
+            showMenu = true;
+        }
+    }
+})();
