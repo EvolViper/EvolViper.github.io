329863f82f69ed72174acb8070ad339c21cb6e08
EvolViper <evol.viper@gmail.com>
2016-10-05 22:11:10 +0300

    menu

--- abyrvalg/js/script.js
+++ abyrvalg/js/script.js
@@ -1,9 +1,15 @@
 //Затемнение верхнего меню
 
-var headerBg = document.querySelector(".header-background")
+var headerBg = document.querySelector(".header-background");
+var scrollPos = window.scrollY;
+var opacity = 0;
 window.onscroll = function() {
-    headerBg.style.opacity = 1;
+    if (window.scrollY > scrollPos) {
+        opacity += 0.04;
+        headerBg.style.opacity = opacity;
+    }
     if (window.scrollY === 0) {
         headerBg.style.opacity = 0;
+        opacity = 0;
     }
 }
