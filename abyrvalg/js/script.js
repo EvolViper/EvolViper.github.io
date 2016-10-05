f71c8affe1c901013213c00269600f921aef27a5
EvolViper <evol.viper@gmail.com>
2016-10-05 22:45:41 +0300

    update

--- abyrvalg/js/script.js
+++ abyrvalg/js/script.js
@@ -1,8 +1,6 @@
 //Затемнение верхнего меню
 
 var headerBg = document.querySelector(".header-background");
-var scrollPos = window.scrollY;
-var opacity = 0;
 window.onscroll = function() {
     if (window.scrollY > 0) {
         headerBg.style.opacity = 1;
