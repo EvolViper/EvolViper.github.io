f745d435b6cd100b6397d299dee5550b02ee79ad
EvolViper <evol.viper@gmail.com>
2016-10-05 22:26:59 +0300

    menu

--- abyrvalg/js/script.js
+++ abyrvalg/js/script.js
@@ -4,12 +4,10 @@ var headerBg = document.querySelector(".header-background");
 var scrollPos = window.scrollY;
 var opacity = 0;
 window.onscroll = function() {
-    if (window.scrollY > scrollPos) {
-        opacity += 0.04;
-        headerBg.style.opacity = opacity;
+    if (window.scrollY > 0) {
+        headerBg.style.opacity = 1;
     }
     if (window.scrollY === 0) {
         headerBg.style.opacity = 0;
-        opacity = 0;
     }
 }
