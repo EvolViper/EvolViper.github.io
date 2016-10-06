a5b69007d17b3f19a5af06e5dd2b5f4252b3c743
EvolViper <evol.viper@gmail.com>
2016-10-06 17:23:21 +0300

    update

--- abyrvalg/js/script.js
+++ abyrvalg/js/script.js
@@ -9,3 +9,23 @@ window.onscroll = function() {
         headerBg.style.opacity = 0;
     }
 }
+
+//Скроллинг
+
+var scrollButton = document.querySelector(".scroll-button");
+var anchor = document.getElementById("about");
+
+scrollButton.onclick = function(evt) {
+    var anchorPosition = anchor.offsetTop;
+    var scrollDistance = 0;
+    evt.preventDefault();
+    scrollDistance = anchorPosition - window.pageYOffset; 
+    for (var i = scrollDistance; i > 0; i--) {
+        setTimeout(function() {
+            window.scrollBy(0, 1);
+            console.log(window.pageYOffset);
+        }, 200);
+    }
+    //window.scrollBy(0, scrollDistance);
+}
+
