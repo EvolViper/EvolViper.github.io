09fa8c7d63a8dabaf346e1759e8124e9e7b5bbd2
EvolViper <evol.viper@gmail.com>
2016-11-02 16:36:56 +0200

    update

--- abyrvalg/js/script.js
+++ abyrvalg/js/script.js
@@ -1,18 +1,10 @@
-//Убираем скроллбар
-
-/*if(window.innerHeight < window.innerWidth) {
-    document.body.style="overflow: hidden";
-}
-  */  
-
 var chromeAndroid = navigator.userAgent.match(/Android/i) && navigator.userAgent.match(/Chrome/i);
 
-var isChrome = (!!window.chrome && !!window.chrome.webstore) || chromeAndroid;
+//var isChrome = (!!window.chrome && !!window.chrome.webstore) || chromeAndroid;
 var isIE = /*@cc_on!@*/false || !!document.documentMode;
 var isEdge = !isIE && !!window.StyleMedia;
 
-
-
+//Отключение дефолтных реакций на ссылки (для Chrome, IE и Eвпу)
 
 function disableDefault(evt) {
     evt.preventDefault();
@@ -42,14 +34,67 @@ var anchorContact = document.querySelector(".main-nav a[href='#contact']");
 var smallLogo = document.querySelector(".logo-link");
 
 window.addEventListener("load", activeLink);
-//window.onscroll = activeLink;
 window.addEventListener("scroll", activeLink);
 
 
+//Подсветка активных пунктов меню
+
 function activeLink() {
-    console.log(window.pageYOffset);
     if (window.pageYOffset == 0) {
-        console.log("Main");
+        anchorAbout.classList.remove("active");
+            anchorWork.classList.remove("active");
+            anchorContact.classList.remove("active");
+            smallLogo.classList.add("active");
+            if (isEdge || isIE) {
+                anchorAbout.addEventListener("click", buttonScroll, activeLink);
+                anchorWork.addEventListener("click", buttonScroll, activeLink);
+                anchorContact.addEventListener("click", buttonScroll, activeLink);
+            }
+    }
+    else if (window.pageYOffset >= (about.offsetTop - 15) && window.pageYOffset <= (about.offsetTop + 150)) {
+        anchorAbout.classList.add("active");
+            anchorWork.classList.remove("active");
+            anchorContact.classList.remove("active");
+            smallLogo.classList.remove("active");
+            if (isEdge || isIE) {
+                anchorAbout.removeEventListener("click", buttonScroll, activeLink);
+                anchorAbout.addEventListener("click", disableDefault);
+                anchorWork.addEventListener("click", buttonScroll, activeLink);
+                anchorContact.addEventListener("click", buttonScroll, activeLink);
+            }
+    }
+    else if (window.pageYOffset >= (work.offsetTop - 15) && window.pageYOffset <= (work.offsetTop + 150)) {
+        anchorAbout.classList.remove("active");
+            anchorWork.classList.add("active");
+            anchorContact.classList.remove("active");
+            smallLogo.classList.remove("active"); 
+            if (isEdge || isIE) {
+                anchorWork.removeEventListener("click", buttonScroll, activeLink);
+                anchorWork.addEventListener("click", disableDefault);
+                anchorContact.addEventListener("click", buttonScroll, activeLink);
+                anchorAbout.addEventListener("click", buttonScroll, activeLink);
+            }
+        
+    }
+    else if (window.pageYOffset >= (contact.offsetTop - 15) && window.pageYOffset <= (contact.offsetTop + 150)) {
+        anchorAbout.classList.remove("active");
+            anchorWork.classList.remove("active");
+            anchorContact.classList.add("active");
+            smallLogo.classList.remove("active");
+            if (isEdge || isIE) {
+                anchorContact.removeEventListener("click", buttonScroll, activeLink);
+                anchorContact.addEventListener("click", disableDefault);
+                anchorWork.addEventListener("click", buttonScroll, activeLink);
+                anchorAbout.addEventListener("click", buttonScroll, activeLink);
+            }
+    }
+}
+
+
+
+/*
+function activeLink() {
+    if (window.pageYOffset == 0) {
         if (!isChrome) {
             anchorAbout.classList.remove("active");
             anchorWork.classList.remove("active");
@@ -71,7 +116,6 @@ function activeLink() {
     }
     else if (window.pageYOffset >= (about.offsetTop - 15) && window.pageYOffset <= (about.offsetTop + 150)) {
         if (!isChrome) {
-            console.log("About");
             anchorAbout.classList.add("active");
             anchorWork.classList.remove("active");
             anchorContact.classList.remove("active");
@@ -94,7 +138,6 @@ function activeLink() {
     }
     else if (window.pageYOffset >= (work.offsetTop - 15) && window.pageYOffset <= (work.offsetTop + 150)) {
         if (!isChrome) {
-            console.log("Work");
             anchorAbout.classList.remove("active");
             anchorWork.classList.add("active");
             anchorContact.classList.remove("active");
@@ -118,7 +161,6 @@ function activeLink() {
     }
     else if (window.pageYOffset >= (contact.offsetTop - 15) && window.pageYOffset <= (contact.offsetTop + 150)) {
         if (!isChrome) {
-            console.log("Contact");
             anchorAbout.classList.remove("active");
             anchorWork.classList.remove("active");
             anchorContact.classList.add("active");
@@ -140,6 +182,7 @@ function activeLink() {
         }
     }
 }
+*/
 
 //Плавный скроллинг
 
@@ -214,7 +257,6 @@ if (window.matchMedia("screen and (min-width: 1199px)").matches) {
 }
 
 
-
 //Внутреннее меню
 
 function buttonScroll(evt) {
@@ -244,13 +286,11 @@ function buttonScroll(evt) {
 anchorAbout.addEventListener("click", buttonScroll, activeLink);
 anchorWork.addEventListener("click", buttonScroll, activeLink);
 anchorContact.addEventListener("click", buttonScroll, activeLink);
+
+//Скролл по стрелке 
+
 scrollButton.onclick = function(evt) {
-    /*var timer = 0;
-    evt.preventDefault();
-    for (var i = 0; i < window.innerHeight / 10; i++) {
-           setTimeout("window.scrollBy(0, 10)", timer * 6);
-           timer++;
-        } */
+
     evt.preventDefault();
     var anchor = evt.target.getAttribute("href");
     var scrollTarget = document.getElementById(anchor.slice(1));
@@ -271,8 +311,9 @@ scrollButton.onclick = function(evt) {
         } 
     }
     else return;
-    
 }
+
+//Скролл по логотипу в навбаре
 
 smallLogo.onclick = function(evt) {
     var timer = 0;
@@ -281,7 +322,6 @@ smallLogo.onclick = function(evt) {
            setTimeout("window.scrollBy(0, -10)", timer * 4);
            timer++;
         } 
-    
 }
 
 //Галерея
